b877b1d85ec387d7b049cb52881739c58ee6ff2d
nnamdi <npn225@nyu.edu>
2022-08-26 17:23:47 -0400

    Removed unnecessary lines of code within the UserDTO2.java file - Michael P. and Nnamdi N.

--- src/main/java/com/revature/dtos/UserDTO2.java
+++ src/main/java/com/revature/dtos/UserDTO2.java
@@ -1,11 +1,6 @@
 package com.revature.dtos;
 
 import lombok.*;
-import org.hibernate.validator.constraints.Length;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 
 @Data
 // @RequiredArgsConstructor
@@ -13,6 +8,6 @@ import javax.validation.constraints.Pattern;
 @NoArgsConstructor
 public class UserDTO2 {
 
-    private @NonNull double balance;
+    private double balance;
 
 }
