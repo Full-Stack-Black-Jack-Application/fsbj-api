054297d5fcda97cb2e76a53d5870c95618754c19
nnamdi <npn225@nyu.edu>
2022-08-26 17:22:57 -0400

    Added a System.out.println that makes it more obvious that the application is running - Michael P. and Nnamdi N.

--- src/main/java/com/revature/FullStackBlackJackApiApplication.java
+++ src/main/java/com/revature/FullStackBlackJackApiApplication.java
@@ -9,6 +9,7 @@ public class FullStackBlackJackApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FullStackBlackJackApiApplication.class, args);
 		System.out.println(org.hibernate.Version.getVersionString());
+		System.out.println("Full-Stack-Black-Jack Application is Ready to Go!!!");
 	}
 
 }
